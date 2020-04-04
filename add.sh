2ab0be0135114d989edee2bd760e67f2eb0177bf
Shane Starcher <shanestarcher@gmail.com>
2020-04-04 11:17:25 -0700

    add script to package and add new chart  to index

---
+++ add.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CHART_PATH=${1:-helm}
+INDEX_PATH=${2:-"helm-charts/docs"}
+if ! hash helm-release 2>/dev/null; then
+    go get github.com/sstarcher/helm-release@master
+fi
+
+helm-release "${CHART_PATH}"
+helm package "${CHART_PATH}/" -d "${INDEX_PATH}/docs"
+helm repo index "${INDEX_PATH}"
+cd "${INDEX_PATH}"
+
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git add docs
+git add index.yaml
+git commit -m "add chart for ${GITHUB_REPOSITORY:-""}"
