53bf5afdb9ba67dd625a9dc5a95cf0426d8535ee
GitHub Action <action@github.com>
2025-11-27 12:45:42 -0500

    Use helm plugin install for helm-release
    Replace go install approach with official Helm plugin installation method.
    This properly installs helm-release as a Helm plugin using:
      helm plugin install https://github.com/sstarcher/helm-release

--- add.sh
+++ add.sh
@@ -5,13 +5,10 @@ CHART_PATH=${1:-helm}
 INDEX_PATH=${2:-"helm-charts/docs"}
 HELM_RELEASE=${HELM_RELEASE:-"master"}
 
-GOPATH=${GOPATH:-${HOME}/go}
-if ! hash helm-release 2>/dev/null; then
-  go install "github.com/sstarcher/helm-release@${HELM_RELEASE}"
+# Install helm-release plugin if not already installed
+if ! helm plugin list | grep -q release; then
+  helm plugin install https://github.com/sstarcher/helm-release
 fi
-
-# Add GOPATH/bin to PATH so helm-release can be found
-export PATH="${GOPATH}/bin:${PATH}"
 
 helm release "${CHART_PATH}"
 helm package "${CHART_PATH}/" -d "${INDEX_PATH}"
